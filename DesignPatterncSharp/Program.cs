84bf793ad59462eec6f08c350319f303c28a079d
Navaladi Vk <navaladivk@outlook.com>
2020-05-26 11:25:27 +1100

    Created a solution to learn design pattern

---
+++ DesignPatterncSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterncSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
