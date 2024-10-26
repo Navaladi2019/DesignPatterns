ecfb27dbf1bc27c2ae5dc33c8eebc9bb9016a4b8
naval <vknavaladi@gmail.com>
2024-10-26 19:04:15 +0530

    factory and abstract factory

---
+++ DesignPatterncSharp/CreationalDesignPattern/AbstractFactoryDesignPattern/AbstractFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterncSharp.CreationalDesignPattern.AbstractFactoryDesignPattern
+{
+    public interface FurnitureFactory
+    {
+        Chair GetChair();
+
+        Sofa GetSofa();
+    }
+
+    public interface Chair
+    {
+
+    }
+
+    public interface Sofa
+    {
+
+    }
+
+    public class VictorianChair : Chair
+    {
+
+    }
+
+    public class ModernChair : Chair { 
+    
+    }
+
+    public class VictorianSofa : Sofa
+    {
+    }
+
+    public class ModernSofa : Sofa { 
+    }
+
+
+    public class VictorianFurnitureFactory : FurnitureFactory
+    {
+        public Chair GetChair()
+        {
+            return new VictorianChair();
+        }
+
+        public Sofa GetSofa()
+        {
+            return new VictorianSofa();
+        }
+    }
+
+    public class ModernFurnitureFactory : FurnitureFactory
+    {
+        public Chair GetChair()
+        {
+            return new ModernChair();
+        }
+
+        public Sofa GetSofa()
+        {
+            return new ModernSofa();
+        }
+    }
+
+    public class ProgramAbdtractClass
+    {
+        void main()
+        {
+            FurnitureFactory factory = new VictorianFurnitureFactory();
+            factory.GetChair();
+            factory.GetSofa();
+        }
+    }
+}
---
+++ DesignPatterncSharp/CreationalDesignPattern/FactoryDesignPattern/Factory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterncSharp.CreationalDesignPattern.FactoryDesignPattern
+{
+    public abstract class Creator
+    {
+        public abstract IProduct CreateProduct();
+    }
+
+    public class CreatorA : Creator
+    {
+        public override IProduct CreateProduct()
+        {
+            return new ProductA();
+        }
+    }
+
+    public class CreatorB : Creator
+    {
+        public override IProduct CreateProduct()
+        {
+            return new ProductB();
+        }
+    }
+
+    public interface IProduct
+    {
+        void DoStuff();
+    }
+
+    public class ProductA : IProduct
+    {
+        public void DoStuff()
+        {
+
+        }
+    }
+
+    public class ProductB : IProduct
+    {
+        public void DoStuff()
+        {
+
+        }
+    }
+
+    public static class FactorySelector
+    {
+        public static Creator GetCreator(string type)
+        {
+            switch (type)
+            {
+                case "A":
+                    return new CreatorA();
+                case "B":
+                    return new CreatorB();
+                default:
+                    throw new NotImplementedException();
+            }
+
+
+        }
+    }
+}
